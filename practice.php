ae86a574cb8572b779466d8400fec641905d7d83
ywtnb35 <watsyk35@outlook.jp>
2022-11-23 05:29:04 +0000

    first commit

---
+++ practice.php
@@ -0,0 +1,34 @@
+<?php
+echo 'hello php!';
+$a=3;
+$b=7;
+echo $a+$b;
+
+$array_month=["1月","2月","3月","4月","5月","6月","7月","8月","9月","10月","11月","12月"];
+echo $array_month[7];
+
+$hello="Hello";
+$name="watanabe";
+$world="'s WWorld!";
+echo$hello.$name.$world;
+
+$tech_boost='tech';
+$tech_boost.='_boost';
+echo$tech_boost;
+
+$calendar_2018 =[
+    "January"=>"1月",
+    "February"=>"2月",
+    "March"=>"3月",
+    "April"=>"4月",
+    "May"=>"5月",
+    "June"=>"6月",
+    "July"=>"7月",
+    "August"=>"8月",
+    "September"=>"9月",
+    "October"=>"10月",
+    "November"=>"11月",
+    "December"=>"12月",];
+    echo$calendar_2018["December"];
+    
+    
